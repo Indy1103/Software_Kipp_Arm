a4c750e1f960b1fd34b29905921fa00aba6bafb5
Induwara <kandapah@ualberta.ca>
2024-01-27 23:59:14 -0700

    update

---
+++ arm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /opt/ros/noetic/setup.bash
+source /ssd/Software_Kipp_Arm/devel/setup.bash 
+source /ssd/Software_Kipp_Arm/install/setup.bash 
+source /ssd/ws_moveit/devel/setup.bash 
